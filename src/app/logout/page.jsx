7da4ccdfdd4324e517d52d135b52d8da1bda2f12
one-person-coder <=>
2025-02-11 00:27:59 -0800

    solving issue

--- src/app/logout/page.jsx
+++ src/app/logout/page.jsx
@@ -1,21 +1,32 @@
 "use client";
 import { ErrorToast, SuccessToast } from "@/components/utils/CustomToasts";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-const Logout = async () => {
+const Logout = () => {
   const router = useRouter();
-  const response = await fetch("/api/logout", {
-    method: "GET",
-  });
-  if (!response.ok) {
-    ErrorToast("Failed to log out.");
-  }
-  const data = await response.json();
 
-  if (data.success) {
-    SuccessToast(data.message)
-    router.push("/login");
-  }
+  useEffect(() => {
+    const logout = async () => {
+      const response = await fetch(`/api/logout`, {
+        method: "GET",
+      });
+
+      if (!response.ok) {
+        ErrorToast("Failed to log out.");
+        return;
+      }
+
+      const data = await response.json();
+
+      if (data.success) {
+        SuccessToast(data.message);
+        router.push("/login");
+      }
+    };
+
+    logout();
+  }, [router]);
 };
 
 export default Logout;
