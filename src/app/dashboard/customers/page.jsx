7204ea1e0b498ac7e743e1108571f2f754d14785
one-person-coder <=>
2025-02-11 03:51:31 -0800

    users section almost completed.

--- src/app/dashboard/customers/page.jsx
+++ src/app/dashboard/customers/page.jsx
@@ -1,14 +1,27 @@
-import CustomerLists from "@/components/Card/CustomerList";
 import UserCards from "@/components/Card/UserCards";
-import React from "react";
+import UserLists from "@/components/Card/UserLists";
+import { ErrorToast } from "@/components/utils/CustomToasts";
 
-const CustomerPage = () => {
+const UsersPage = async () => {
+  const response = await fetch(`${process.env.MAIN_API}/api/users/status`, {
+    method: "GET",
+  });
+  const userStatus = await response.json();
+
+  if (!userStatus.success) {
+    ErrorToast("User Cannot Fetch Server Error");
+  }
+
+  if (!userStatus.success) {
+    ErrorToast("User Cannot Fetch Server Error");
+  }
+
   return (
     <div className="custom-width">
-      <UserCards />
-      <CustomerLists />
+      <UserCards data={userStatus} />
+      <UserLists data={userStatus} />
     </div>
   );
 };
 
-export default CustomerPage;
+export default UsersPage;
