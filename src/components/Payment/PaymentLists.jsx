bf11d2a9cb4a3796e5474af919b5481df86a2d29
one-person-coder <=>
2025-03-10 13:25:27 -0700

    removed payment search option

--- src/components/Payment/PaymentLists.jsx
+++ src/components/Payment/PaymentLists.jsx
@@ -398,15 +398,6 @@ export default function PaymentLists({ data, customerId, permissions }) {
                   Refresh
                 </Button>
               </div>
-
-              <div className="relative">
-                <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
-                <Input
-                  onChange={handleSearchChange}
-                  placeholder="Search payment..."
-                  className="w-full pl-9 sm:w-[300px] bg-gray-50 border-0 ring-1 ring-gray-200"
-                />
-              </div>
             </div>
 
             {/* Table */}
